4a08a2c1a1e8201597c203238b5869029e327a22
eliellima00 <eliellimamatos@gmail.com>
2025-11-04 22:03:11 -0400

    feat: adicionando script de ingest

---
+++ README.md
@@ -0,0 +1,41 @@
+
+Este projeto é a resolução do primeiro desafio técico do MBA em Engenharia de Sofware com I.A da FullCycle.
+
+O objetivo é realizar o embedding de um documento em PDF e depois realizar consultas no banco de dados vetorial PG Vector.
+
+## Tecnologias utilizadas
+
+- Linguagem: Python
+- Framework: Langchain
+- Banco de dados: Postgres + Extenção PG Vector
+- Execução do banco de dados: Docker & Docker Compose
+## Como executar
+
+Abaixo listo um passo a passo de como executar este projeto da melhor maneira no seu ambiente local
+
+### 1 - Ambiente
+
+Antes de executar o projeto precisamos configurar o ambiente. Certifique de ter o docker e o python instalado.
+No momento estou usando o python 3.10.12
+
+Após instalar, configure um ambiente virtual para o python, basta executar o comando `python3 -m venv venv`.
+
+Para ativar o ambiente virtual, execute o comando `source venv/bin/activate` na raiz do projeto.
+
+Após executar este comando, já estará no ambiente python isolado usado o venv.
+
+Depois certifique de criar o arquivo `.env` com base no `.env.example` e preencher todas as variáveis.
+
+### 2 - Subir o container do banco de dados
+Basta dar um docker compose up e vai subir o banco de dados com as configurações do `docker-compose.yml`
+
+Para testar a conexão, use seu banco de dados de preferencia, no meu caso usei dbeaver com as seguintes configurações
+![!\[\[dbeaver rag challenge.png\]\]](assets/image.png)
+
+### 3 - Execução do Ingest dos Documentos
+Para executar o script de ingest dos documentos, usando o comando `python3 src/ingest.py`.
+
+Se ocorrer tudo certo, vai exibir a mensagem :
+> EMBEDDING FINISHED WITH SUCCESS
+
+
--- src/ingest.py
+++ src/ingest.py
@@ -1,1 +1,66 @@
-#todo: Criar script de ingestão do PDF
+import os 
+from pathlib import Path
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
+
+load_dotenv()
+
+required_environment = [
+    "OPENAI_API_KEY",
+    "OPENAI_EMBEDDING_MODEL",
+    "DATABASE_URL", 
+    "PG_VECTOR_COLLECTION_NAME",
+    "PDF_PATH"
+]
+
+for k in (required_environment):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
+current_dir = Path(__file__).parent
+root_dir = current_dir.parent
+pdf_path =  os.getenv("PDF_PATH")
+
+if not pdf_path:
+    raise RuntimeError("PDF PATH cannot be null")
+
+document_path = str(root_dir) + '/' + pdf_path
+
+docs = PyPDFLoader(document_path).load()
+
+chunks = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 150,
+    add_start_index=False
+).split_documents(docs) 
+
+if not chunks:
+    raise SystemExit(0)
+
+
+enriched = []
+for d in chunks:
+    meta = {k: v for k, v in d.metadata.items() if k not in ("", None)}
+    new_doc = Document(
+        page_content=d.page_content,
+        metadata=meta
+    )
+    enriched.append(new_doc)
+
+ids = [f"doc-{i}" for i in range(len(enriched))]
+
+embeddingsDocuments = OpenAIEmbeddings(model=os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"))
+
+store = PGVector(
+    embeddings=embeddingsDocuments,
+    collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME", "rag-collection"),
+    connection=os.getenv("DATABASE_URL"),
+    use_jsonb=True
+)
+
+store.add_documents(documents=enriched, ids=ids)
+print("EMBEDDING FINISHED WITH SUCCESS")
